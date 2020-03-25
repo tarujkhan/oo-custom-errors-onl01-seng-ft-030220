bbfb20fdc82674cb9ee7dd18b158bd124e7875dd
taroj khan <tarujkhan@yahoo.com>
2020-03-25 20:45:26 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,7 +8,12 @@ class Person
   def get_married(person)
     self.partner = person
     person.partner = self
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
   end
+end 
 
  class PartnerError < StandardError
   end
